da76a6752933fbe73a7e090d5b3579f95dc63d13
Yusuf Sahin <yusuf@yusufsahin.net>
2025-01-05 00:08:54 +0300

    first commit

---
+++ basics/script.js
@@ -0,0 +1,4 @@
+// Butonun click olayını dinliyoruz
+document.getElementById("showMessageBtn").addEventListener("click", function() {
+    alert("Bizimle iletişime geçtiğiniz için teşekkür ederiz!");
+});
